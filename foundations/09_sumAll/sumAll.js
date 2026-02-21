60a29ffcfc176e3880b427e21782d48f1cbc37bf
Fabian Bruelisauer <fbruelisa@ethz.ch>
2026-02-21 15:20:46 +0100

    task: foundations/09 done

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    if(!Number.isInteger(a) || !Number.isInteger(b)) return 'ERROR';
+
+    let lower = Math.min(a, b)
+    let upper = Math.max(a, b)
+
+    if(lower < 0) return 'ERROR';
+
+    let sum = 0;
+    for(let i = lower; i <= upper; i++) {
+        sum += i
+    }
+    return sum;
 };
 
 // Do not edit below this line
