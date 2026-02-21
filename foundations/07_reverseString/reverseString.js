286c9febb6876ac17f72df878c6ca3190e9b0bb2
Fabian Bruelisauer <fbruelisa@ethz.ch>
2026-02-21 14:56:10 +0100

    task: foundations/07 done

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let res = "";
+    for(let i = 0; i < str.length; i++) {
+        res = str.at(i) + res;
+    }
+    return res;
 };
 
 // Do not edit below this line
