61e3ee517e34b74e44792030ad2c3f0d210c8f17
Fabian Bruelisauer <fbruelisa@ethz.ch>
2026-02-22 17:00:26 +0100

    task: foundations/13 done

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replaceAll(",", "").replaceAll(" ", "").replaceAll(".", "").replaceAll("!", "").toLowerCase();
+    let revert = "";
+    for(let i = 0; i < str.length; i++) {
+        revert = str.at(i) + revert;
+    }
+    return revert === str;
 };
 
 // Do not edit below this line
