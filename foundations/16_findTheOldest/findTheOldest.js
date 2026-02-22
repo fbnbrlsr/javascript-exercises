60755d4176921ecf8f2764e880d48a64363316d9
Fabian Bruelisauer <fbruelisa@ethz.ch>
2026-02-22 17:20:47 +0100

    task: foundations/16 done

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+function addDeath(person) {
+    if(!("yearOfDeath" in person)){
+        person.yearOfDeath = new Date().getFullYear();
+    }
+}
 
+const findTheOldest = function(people) {
+    people.forEach(person => addDeath(person));
+    let ages = people.map((person) => ({...person, age: person.yearOfDeath - person.yearOfBirth}));
+    return ages.reduce((oldest, person) => {
+        if(oldest.age < person.age) return person;
+        return oldest;
+    }, {age: -1})
 };
 
 // Do not edit below this line
